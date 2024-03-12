875d09f33795397bec2557358f880726effca439
t.onwuchekwa cos101 <treasure32109@gmail.com>
2024-03-12 19:29:39 +0100

    Encore

---
+++ week-1/practice-1.py
@@ -0,0 +1,14 @@
+#Integer number
+num = 100
+print(num)
+print("Data type of num is", type(num))
+
+#float number
+fnum = 34.45
+print(fnum)
+print("Data type of num is", type(fnum))
+
+#complex number
+cnum = 3 + 4j
+print(cnum)
+print("Data type of num is", type(cnum))
---
+++ week-1/practice-2.py
@@ -0,0 +1,12 @@
+#python program to print strings and type
+str1 = "Hi I'm Mathew I'm a string."
+str2 = "Hi I'm Precious I'm also a string.",
+
+
+#displaying string str1 and it's type
+print(str1)
+print(type(str1))
+
+#displaying string str2 and it's type
+print(str2)
+print(type(str2))
